6ba8e24987447c64cf02a63a02f9a823f8348f84
Faiz-js <faiz.momin18112003@gmail.com>
2026-03-08 16:27:45 +0530

    testing out whole CICD pipeline

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const btn = document.getElementById("btn");
+const dblBtn = document.getElementById("dbl-btn");
 
 btn.addEventListener("click", () => {
   alert("button was clicked");
+});
+
+dblBtn.addEventListener("dblclick", () => {
+  alert("button was double clicked");
 });
